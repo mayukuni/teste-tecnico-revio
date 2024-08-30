c4b81954d4459455f6694cafef6b5c1777a8af89
Mayumi Kuniyoshi <cinthya.m.kuniyoshi@gmail.com>
2024-08-29 21:30:46 -0300

    cria uma instância do chrome para abrir a página

---
+++ script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service = Service()
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://www.omelete.com.br/")
